219e18bcae434133cf1ef0bf26e91128fd7d2736
shwzang <shwzang@nate.com>
2020-06-25 19:53:11 +0900

    [심현우 연구원] base_URL 수정

--- front-end/src/services/DiaryService.js
+++ front-end/src/services/DiaryService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: `${process.env.VUE_APP_SERVER_URL}`,
   withCredentials: false, // This is the default
   headers: {
     Accept: 'application/json',
